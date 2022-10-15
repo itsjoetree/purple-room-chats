b3f457b84cf69f24bffaa57ba6eb1705cb60798f
itsjoetree <84997596+itsjoetree@users.noreply.github.com>
2022-10-15 17:47:15 -0400

    changes

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      <div className={`${window?.matchMedia ? (window?.matchMedia('(display-mode: standalone)').matches ? "index__pwa" : "index") : 'index'}`} style={{marginBottom: "3em", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+      <div className={`${window?.matchMedia ? (window?.matchMedia('(display-mode: standalone)').matches ? "index__pwa" : "index") : ''}`} style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
         <div style={{display: "flex", gap: "1em"}}>
           <h1 className="index__heading" style={{fontSize: "3rem", fontFamily: "headingFont"}}>
             purple room chats
